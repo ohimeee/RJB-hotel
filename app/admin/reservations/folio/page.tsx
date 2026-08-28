1a166152dedf9cc5bebd421c4a52384b7c9cbe35
L1Pendz <2007rlee@gmail.com>
2026-08-28 09:45:53 +0800

    create folio page within reservations folder

---
+++ app/admin/reservations/folio/page.tsx
@@ -0,0 +1,13 @@
+const FolioPage = () => {
+  return (
+    <>
+      <h1 className="mb-4 text-3xl font-bold">
+        Folio Page
+      </h1>
+
+      <p>Folio form goes here.</p>
+    </>
+  );
+}
+
+export default FolioPage;
